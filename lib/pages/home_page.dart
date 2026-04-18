326156694845d155779e650de49838399ca22e71
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-04-17 22:25:48 -0500

    logica inicial

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,16 +8,31 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Quizbrain quizbrain = Quizbrain();
+  List<Widget> score = [];
 
   void checkAnswer(bool userAnswer) {
     bool correctAnswer = quizbrain.getQuestionAnswer();
     if (userAnswer == correctAnswer) {
+      score.add(itemScore((quizbrain.questionIndex + 1).toString(), true));
       print("La respuesta es CORRECTA");
     } else {
+      score.add(itemScore((quizbrain.questionIndex + 1).toString(), false));
       print("La respuesta es INCORRECTA");
     }
     quizbrain.nextQuestion();
     setState(() {});
+  }
+
+  Widget itemScore(String numberQuestion, bool isCorrect) {
+    return Row(
+      children: [
+        Text("$numberQuestion:", style: TextStyle(color: Colors.white)),
+        Icon(
+          isCorrect ? Icons.check : Icons.close,
+          color: isCorrect ? Colors.greenAccent : Colors.redAccent,
+        ),
+      ],
+    );
   }
 
   @override
@@ -71,6 +86,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: score),
         ],
       ),
     );
